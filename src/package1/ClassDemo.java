7131817480c78ed5c4f30a4430047a72d9729ad4
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2021-01-13 18:41:47 -0500

    last update from remote master branch

--- src/package1/ClassDemo.java
+++ src/package1/ClassDemo.java
@@ -7,6 +7,7 @@ public class ClassDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("here are my updates about DemoClass");
+		System.out.println("A colaborator updated");
 	}
 	
 }
